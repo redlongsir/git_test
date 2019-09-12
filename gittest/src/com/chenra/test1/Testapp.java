086106fe64a40665917e10f0cd9a44bca30fe8d2
chenra <redlongsir@163.com>
2019-09-12 17:21:39 +0800

    修改

--- gittest/src/com/chenra/test1/Testapp.java
+++ gittest/src/com/chenra/test1/Testapp.java
@@ -4,4 +4,8 @@ public class Testapp {
     public static void main(String[] args){
         System.out.println("GIT 测试");
     }
+
+    public void test(){
+        System.out.println("test()");
+    }
 }
